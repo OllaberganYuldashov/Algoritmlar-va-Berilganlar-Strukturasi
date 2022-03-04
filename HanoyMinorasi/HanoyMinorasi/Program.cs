43bdac3096610a0ba3ba441051be676be390bed1
Allabergan <ollaberganyuldashov@gmail.com>
2022-03-05 01:28:15 +0300

    jbj
    vkj

---
+++ HanoyMinorasi/HanoyMinorasi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HanoyMinorasi
+{
+    class Program
+    {
+
+        static void Hanoy(int n, char A, char B, char C) 
+        {
+            
+            if (n == 1)
+            {
+                Console.WriteLine(A.ToString() + " dan " + B.ToString() + " ga.");
+            }
+            else {
+                Hanoy(n - 1, A, C, B);
+
+                Console.WriteLine(A.ToString() + " dan " + B.ToString() + " ga.");
+                Hanoy(n - 1, C, B, A);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Hanoy(n, 'A', 'B', 'C');
+            Console.ReadKey();
+
+        }
+    }
+}
